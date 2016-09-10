36ca2f154fee70439fcae3ee7d198d9fa31839f2
johnnybionic <jdavddlo@gmail.com>
2016-09-10 15:11:07 +0100

    no need to initialise mocks

--- dispatcher/src/test/java/com/johnny/dispatcher/service/MessageServiceTest.java
+++ dispatcher/src/test/java/com/johnny/dispatcher/service/MessageServiceTest.java
@@ -1,18 +1,17 @@
 package com.johnny.dispatcher.service;
 
-import static org.hamcrest.CoreMatchers.isA;
-import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.verify;
+
+import com.johnny.dispatcher.domain.Document;
+import com.johnny.dispatcher.domain.DocumentRequest;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jms.core.JmsTemplate;
@@ -20,41 +19,36 @@ import org.springframework.jms.core.MessageCreator;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.johnny.dispatcher.domain.Document;
-import com.johnny.dispatcher.domain.DocumentRequest;
-import com.johnny.dispatcher.domain.Task;
-
 @ActiveProfiles("junit")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class MessageServiceTest {
 
-	private static final Long DOCUMENT_ID = 42L;
-	private static final String IDENTIFIER = "corr-id-1";
+    private static final Long DOCUMENT_ID = 42L;
+    private static final String IDENTIFIER = "corr-id-1";
 
-	@InjectMocks
-	private MessageServiceJms service;
-	
-	@Mock
-	private JmsTemplate jmsTemplate;
+    @InjectMocks
+    private MessageServiceJms service;
 
-	@Value("${document.queue.request}") 
-	private String documentRequestQueue;
-	
-	@Before
-	public void setUp() {
-		MockitoAnnotations.initMocks(this);
-		service.setDocumentRequestQueue(documentRequestQueue);
-	}
-	
-	@Test
-	public void test() {
-		
-		Document document = new Document(DOCUMENT_ID, null, null);
-		DocumentRequest documentRequest = new DocumentRequest(IDENTIFIER, document , null);
-		service.sendMessage(documentRequest );
-		
-		verify(jmsTemplate).send(anyString(), any(MessageCreator.class));
-	}
+    @Mock
+    private JmsTemplate jmsTemplate;
+
+    @Value("${document.queue.request}")
+    private String documentRequestQueue;
+
+    @Before
+    public void setUp() {
+        service.setDocumentRequestQueue(documentRequestQueue);
+    }
+
+    @Test
+    public void test() {
+
+        final Document document = new Document(DOCUMENT_ID, null, null);
+        final DocumentRequest documentRequest = new DocumentRequest(IDENTIFIER, document, null);
+        service.sendMessage(documentRequest);
+
+        verify(jmsTemplate).send(anyString(), any(MessageCreator.class));
+    }
 
 }
